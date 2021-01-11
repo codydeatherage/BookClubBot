cf4c96dd747c034b24f1a7b430130f6d6df0c656
codydeatherage <cdeatherage14@gmail.com>
2021-01-11 08:42:58 -0800

    added role manipulation

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
 // require the discord.js module
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
-import { Client, Message, MessageEmbed} from 'discord.js';
+import { Client, Message, MessageEmbed, Guild} from 'discord.js';
 const auth = require('./auth.json');
 const ffmpeg = require('ffmpeg');
 
 // create a new Discord client
 const client = new Client();
+
 let isReady = true;
+
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
 client.once('ready', () => {
+    const guild = client.guilds.cache.get(auth.server);
+    if(guild.roles.cache.find(r => r.name === 'Discussion Leader') === undefined){
+        console.log('Role not found, creating role...');
+        guild.roles.create({
+            data: {
+                name: 'Discussion Leader',
+                color: 'GOLD'
+            },
+            reason: 'Leader of weekly book discussions'     
+        })
+        .then(console.log('Role Created'));
+    }
+    else{
+        //gets id of role named 'Discussion Leader'
+        console.log(guild.roles.cache.find(r => r.name === 'Discussion Leader').id);
+        console.log(guild.roles.cache.find(r => r.id === '798213168421404702').name);
+        console.log('Role already exists!');
+ 
+    }
+    client.user.setPresence({ type: "CUSTOM_STATUS", activity: { name: 'Next Meeting: 11/30' }, status: 'online' })
+    .catch(console.error);
     console.log('Ready!');
+})
 
-    
 
-client.user.setPresence({ type: "CUSTOM_STATUS", activity: { name: 'Next Meeting: 11/30' }, status: 'online' })
-/* .then(console.log) */
-.catch(console.error);
-});
 let nextMeeting = '11/30';
 let currentLead = 'Cody';
 
 client.on('message', message => {
     let senderID = message.author.id;
-    console.log('MESSAGE RECEIVED from', message.author.id);
+    //console.log('MESSAGE RECEIVED from', message.author.id);
+    console.log('Message:', message.content);
     if (isReady && message.content === '+gong'){
         isReady = false;
         const voiceChannel = message.member.voice.channel;
@@ -50,9 +70,32 @@ client.on('message', message => {
                 /* .then(console.log) */
                 .catch(console.error);           
     }
-    else if(isReady && message.content.includes('+leader')){
-        m
-    }   
+    else if(isReady && message.content.includes('+set leader')){
+        const guild = client.guilds.cache.get(auth.server);
+        const {cache} = message.guild.members;
+        //command format: +set leader <@newLeader>
+        let newLeaderID = message.content.slice(message.content.indexOf('r'));
+        newLeaderID = newLeaderID.slice(2);
+        newLeaderID = newLeaderID.replace(/[<@!>]/g, '');
+        console.log('newLeaderID:', newLeaderID);
+        
+        //gives the role id of the 'Discussion Leader' role
+        let leaderRoleID = guild.roles.cache.find(r => r.name === 'Discussion Leader').id
+        //gives the current holder of the role
+        let prevLeadID = cache.find(r => r._roles.includes(leaderRoleID)).user.id; 
+        console.log('prevLeadID:', prevLeadID);
+        //if theres is someone with the role currently
+        if(prevLeadID){
+            //remove role from previous leader
+            cache.find(m => m.user.id === prevLeadID).roles.remove(leaderRoleID);
+            //assign role to new selected leader
+            cache.find(m => m.user.id === newLeaderID).roles.add(leaderRoleID);
+        }else{
+            //assign role to new selected leader
+            cache.find(m => m.user.id === newLeaderID).roles.add(leaderRoleID);
+        }
+    }  
+
     else if(isReady && message.content === '+help'){
         const embed = new MessageEmbed()
         .setTitle('Right away!')
